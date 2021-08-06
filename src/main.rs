0c19af12de51b7a5224c11c5d67cb871b9edba48
Chris Goller <goller@gmail.com>
2021-08-06 11:01:38 -0500

    perf: buffer stdout and reuse encoding buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use structopt::StructOpt;
 use uuid::Uuid;
 
+use std::io::Write;
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "goop", about = "Create many guids")]
 struct Opt {
@@ -11,9 +13,13 @@ struct Opt {
 
 fn main() {
     let opt = Opt::from_args();
+
+    let mut stdout = std::io::BufWriter::new(std::io::stdout());
+    let mut buf = Uuid::encode_buffer();
+
     (0..opt.num).into_iter().for_each(|_| {
         let uuid = Uuid::new_v4();
-        let output = uuid.to_hyphenated().to_string();
-        println!("{}", output);
+        let output = uuid.to_hyphenated_ref().encode_lower(&mut buf);
+        writeln!(&mut stdout, "{}", output).unwrap();
     });
 }
